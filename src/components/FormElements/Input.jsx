b815adca7ce49c18b465e70800feb5f48274ed59
belhssanklouz <belhssanklouz@gmail.com>
2022-01-27 16:26:47 +0100

    myycommmit

--- src/components/FormElements/Input.jsx
+++ src/components/FormElements/Input.jsx
@@ -4,7 +4,7 @@ import { TextField } from '@mui/material'
 
 const Input = (props) => {
     return (
-        <TextField variant='filled' style={{backgroundColor:'red !important;' }} name={props.name} label={props.label} defaultValue={props.value} onChange={props.onChange} type={props.type} hidden={true} required={props.required}/>
+        <TextField variant='filled' style={props.style} name={props.name} label={props.label} defaultValue={props.value} onChange={props.onChange} type={props.type} hidden={true} required={props.required}/>
             
         
     )
--- src/pages/devis/addDevis.jsx
+++ src/pages/devis/addDevis.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { UseForm,Form } from '../../components/form-hook/UseForm';
 import Input from '../../components/FormElements/Input';
 import Button from '@mui/material/Button';
 import {FormControl, FormControlLabel, FormLabel, Grid , RadioGroup,Radio,Autocomplete,TextField} from '@mui/material';
-
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import { useSelector } from 'react-redux';
+import IconButton from '@mui/material/IconButton'
 
 
 
 const AddDevis = () => {
-
+    const [inputIncr,setInputIncr] = useState([{product:'',qnt:0}]);
     const products = useSelector(state=>state.ProductReducer.products) || [];
     const mappedProducts = products.map(data=>data.name) || [];
-    console.log(products);
+
+    const handleAddInput = () => {
+            setInputIncr([...inputIncr,{product:'',qnt:0}])
+        //  setInputIncr(inputIncr.concat({product:'',qnt:0}))
+    }
+    const handleRemoveInput = (index) =>{
+        // const val = [...inputIncr];
+        // val.splice(1,1)
+        // console.log(val)
+        // console.log(index,' deleted')
+        // setInputIncr(val);
+    }
 
     const initialValues ={
         numerodevis:'',
@@ -39,32 +52,41 @@ const AddDevis = () => {
 
   return <Form onSubmit={submitHandler}>
         <Grid container>
-            <Grid item xs={12}>
+            <Grid  item xs={6}>
                 <Input name='numeroDevis' label='Numero de devis' value={values.name} onChange={handleInputChange} required/>
-                        
-                <Input name='client' label='Client' value={values.client} onChange={handleInputChange} type='text' />
+                <Input name='client' label='Client' value={values.client} onChange={handleInputChange} type='text' />         
+                <Input name='validity' label='Date de validité' value={values.email} onChange={handleInputChange} type='text' />
+                <Input name='validity' label='Date de validité' value={values.email} onChange={handleInputChange} type='text' />
+                <Input name='validity' label='Date de validité' value={values.email} onChange={handleInputChange} type='text' />
+            </Grid>
+            <Grid item xs={6}>
+                {inputIncr.map((el,index)=>(
+               <div style={{display:'flex'}} key={index}>   
                 <Autocomplete
-                    multiple
-                    id="tags-standard"
+                    id="product"
+                    sx={{ width: 200 }}
                     options={mappedProducts}
                     getOptionLabel={(option) => option}
-                    defaultValue={[mappedProducts[0]]}
                     renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        variant="filled"
-                        label="Produits"
-                        placeholder="Choisir vos produits"
-                    />
+                        <TextField variant="filled" {...params} label="Produit" margin="normal" onChange={handleInputChange} />
                     )}
-                />                
-      <Input name='validity' label='Date de validité' value={values.email} onChange={handleInputChange} type='text' />
-                <Input name='validity' label='Date de validité' value={values.email} onChange={handleInputChange} type='text' />
-                <Input name='validity' label='Date de validité' value={values.email} onChange={handleInputChange} type='text' />
-
-
-            </Grid>
+                />
+                <Input style={{width:200}} name='quantite' label='Quantité' onChange={handleInputChange} type='number' />   
+                <div>
+                <IconButton onClick={()=>handleAddInput()}>
+                    <AddIcon />
+                </IconButton>
+                </div>
+                <div>
+                <IconButton onClick={()=>handleRemoveInput(el,index)}>
+                    <RemoveIcon />
+                </IconButton>  
+                </div>
+                </div>  ))}
+                </Grid>
+           
                 
+              
             </Grid>
             <Button variant="contained" type='submit'>Submit</Button>
         </Form> ;
